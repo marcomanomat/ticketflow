6bb78da51a4679aac7adabb0390a45dd1b1f8923
Marco Manomat <marco.manomat@gmail.com>
2018-08-02 19:00:47 -0400

    finish login for members

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,6 @@
 class ApplicationController < ActionController::Base
-	# helper_method :current_member
 
- #  def sign_in(member)
- #    cookies.permanent[:remember_token] = member.remember_token
- #    self.current_member = member
- #  end
-
- #  def signed_in?
- #    !current_member.nil?
- #  end
-
- #  def current_member=(member)
- #    @current_member = member
- #  end
-
- #  def current_member
- #    @current_member ||= Member.find_by_remember_token(cookies[:remember_token])
- #  end
-
- #  def sign_out
- #    self.current_member = nil
- #    cookies.delete(:remember_token)
- #  end
+	protect_from_forgery with: :exception
+	include SessionsHelper
+	
 end
